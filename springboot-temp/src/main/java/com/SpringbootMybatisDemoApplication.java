a80882c2eca9355ee3966c83ef6bb92f17377cd1
feigege <312012733@qq.com>
2018-12-28 00:58:16 +0800

    d

--- springboot-temp/src/main/java/com/SpringbootMybatisDemoApplication.java
+++ springboot-temp/src/main/java/com/SpringbootMybatisDemoApplication.java
@@ -1,16 +1,32 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootMybatisDemoApplication
+@EnableAutoConfiguration
+public class SpringbootMybatisDemoApplication extends SpringBootServletInitializer
 {
     
-    public static void main(String[] args) throws NoSuchFieldException, SecurityException, NoSuchMethodException
+    // public static void main(String[] args) throws NoSuchFieldException,
+    // SecurityException, NoSuchMethodException
+    // {
+    // ConfigurableApplicationContext context =
+    // SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+    //
+    // }
+    
+    public static void main(String[] args)
     {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
-        
+        SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(SpringbootMybatisDemoApplication.class);
     }
 }
